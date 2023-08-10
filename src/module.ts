b9e0b3db4f1a183ca5a336f78e279fab0c75d259
Sam K <33263129+samk-dev@users.noreply.github.com>
2023-08-10 14:25:20 +0200

    chore: added method to route name

--- src/module.ts
+++ src/module.ts
@@ -19,7 +19,7 @@ export default defineNuxtModule<ModuleOptions>({
     const resolver = createResolver(import.meta.url);
     addServerHandler({
       route: "/api/hello/[name]",
-      handler: resolver.resolve("./runtime/server/api/hello/[name].ts"),
+      handler: resolver.resolve("./runtime/server/api/hello/[name].get.ts"),
     });
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve("./runtime/plugin"));
